9a9e77ec44dbfd94335080024c6750d6242938bf
VibinRaj-0407 <vibinraj20003@gmail.com>
2023-11-07 19:56:42 +0530

    adding stringutil dependancy

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,20 @@
-package clinic.programming.training;
+ package clinic.programming.training;
+import java.util.*;
 
 public class Application {
-    
-    public Application() {
-        System.out.println ("Inside Application");
-    }
+    public int countWords(String word){
+	String[] s = word.split(" ");
+	return s.length;
+}
+	public Application() {
+        	System.out.println ("Inside Application");
+	}
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	int c = app.countWords("hello world");
+	System.out.println(c);
     }
 }
